78f8adcccc3144e073fe3a40ce1134b4b6b7a0e4
Tatsuo Kaniwa <tatsuo@kaniwa.biz>
2014-04-05 15:21:17 +0900

    handle all request

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  get '*' => 'pages#home'
+  get '(*foo)' => 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
